1f589f1db9f8677c0c6598aa849a154a8e387138
Jesil OT <toboroweijesil@gmail.com>
2021-09-14 07:04:46 -1100

    init master branch

---
+++ app/src/main/java/com/jesil/toborowei/marvelunlimited/SplashActivity.kt
@@ -0,0 +1,11 @@
+package com.jesil.toborowei.marvelunlimited
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
